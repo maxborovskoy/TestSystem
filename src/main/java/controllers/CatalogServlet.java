383abf6bd6ebccafe2d74d843020f33f5f9447c5
Guba Dmitriy <gudmian@gmail.com>
2018-03-14 14:17:42 +0300

    Added first servlet. Created sql_queries.properties. Created web.xml configuration.

---
+++ src/main/java/controllers/CatalogServlet.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CatalogServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("catalog.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/webapp/catalog.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dmitriy
+  Date: 14.03.18
+  Time: 14:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Catalog</title>
+</head>
+<body>
+
+
+
+    <H1>Catalog</H1>
+
+</body>
+</html>
