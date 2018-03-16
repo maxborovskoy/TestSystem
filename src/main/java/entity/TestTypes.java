127de946a808a8c09ff8d39e986db97c1e9b63b5
Maksim_Borovskoi <maksim_borovskoi@epam.com>
2018-03-16 13:35:38 +0300

    Simplified ENUMs

--- src/main/java/entity/TestTypes.java
+++ src/main/java/entity/TestTypes.java
@@ -1,16 +1,7 @@
 package entity;
 
 public enum TestTypes {
-    MATH( "Math" ),
-    PHYSICS( "Physics" ),
-    ENGLISH( "English" );
-
-    private String name;
-
-    TestTypes (String name){
-        this.name = name;
-    }
-    public String getName(){
-        return this.name;
-    }
+    MATH,
+    PHYSICS,
+    ENGLISH
 }
