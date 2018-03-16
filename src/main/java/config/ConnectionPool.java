35eb6a3a51b127480617df3bf94e24b49334ff34
oceankind <koroteckiiba@mail.ru>
2018-03-16 13:23:33 +0300

    add constant fields for connection parameters from properties

--- src/main/java/config/ConnectionPool.java
+++ src/main/java/config/ConnectionPool.java
@@ -8,8 +8,15 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class ConnectionPool {
+    private static final ResourceBundle RESOURCE = ResourceBundle.getBundle("sql_queries");
+    private static final String H2_URL = RESOURCE.getString("h2.url");
+    private static final String H2_USER = RESOURCE.getString("h2.user");
+    private static final String H2_PASS = RESOURCE.getString("h2.password");
+    private static final int MAX_CONNECTION = Integer.parseInt(RESOURCE.getString("h2.maxConnection"));
     private List<Connection> freeConnections;
     private static ConnectionPool instancePool;
+
+
 
     private ConnectionPool() {
         freeConnections = new ArrayList<>();
@@ -18,13 +25,8 @@ public class ConnectionPool {
     public static ConnectionPool getInstance() throws SQLException {
         if (instancePool == null) {
             instancePool = new ConnectionPool();
-            ResourceBundle resource = ResourceBundle.getBundle("sql_queries");
-            String url = resource.getString("h2.url");
-            String user = resource.getString("h2.user");
-            String password = resource.getString("h2.password");
-            int maxConn = Integer.parseInt(resource.getString("h2.maxConnection"));
-            for (int i = 0; i < maxConn; i++) {
-                instancePool.freeConnections.add(DriverManager.getConnection(url, user, password));
+            for (int i = 0; i < MAX_CONNECTION; i++) {
+                instancePool.freeConnections.add(DriverManager.getConnection(H2_URL, H2_USER, H2_PASS));
             }
         }
         return instancePool;
