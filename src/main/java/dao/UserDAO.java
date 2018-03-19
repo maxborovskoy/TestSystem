789a474abdcd26a13cc28602914f8e24fb8abd34
Dmitrii_Dementev <Dmitrii_Dementev@epam.com>
2018-03-19 19:23:36 +0300

    changed UserDAO

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -12,123 +12,78 @@ import java.util.List;
 public class UserDAO extends AbstractDAO<User, Long> {
     @Override
     public void add(User entity) {
-        Connection con = null;
-        PreparedStatement st = null;
-        try {
-            con = pool.getConnection();
-            st = con.prepareStatement(sqlQueries.getString("ADD_USER"));
-            st.setLong(1, entity.getId()); // Where do we get this?
-            st.setString(2, entity.getName());
-            st.setString(3, entity.getPassword());
-            st.setBoolean(4, entity.getTutor());
+        try (
+                Connection con = pool.getConnection();
+                PreparedStatement st = con.prepareStatement(sqlQueries.getString("ADD_USER"));
+        ) {
+            st.setString(1, entity.getName());
+            st.setString(2, entity.getPassword());
+            st.setBoolean(3, entity.getTutor());
             st.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (st != null)
-                    st.close();
-                if (con != null)
-                    con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     @Override
     public User get(Long id) {
-        Connection con = null;
-        PreparedStatement st = null;
-        ResultSet rs = null;
-        User a = null;
-        try {
-            con = pool.getConnection();
-            st = con.prepareStatement(sqlQueries.getString("GET_USER"));
+        try (
+                Connection con = pool.getConnection();
+                PreparedStatement st = con.prepareStatement(sqlQueries.getString("GET_USER"));
+        ) {
             st.setLong(1, id);
-            rs = st.executeQuery();
-            if (rs.next()) {
-                a = new User();
-                a.setId(rs.getLong("id"));
-                a.setName(rs.getString("name"));
-                a.setPassword(rs.getString("pass"));
-                a.setTutor(rs.getBoolean("isTutor"));
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    String name = rs.getString("name");
+                    String pass = rs.getString("pass");
+                    boolean tutor = rs.getBoolean("isTutor");
+                    User user = new User(name, pass, tutor);
+                    user.setId(id);
+                    return user;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null)
-                    rs.close();
-                if (st != null)
-                    st.close();
-                if (con != null)
-                    con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
-        return a;
+        return null;
     }
 
-    @Override
     public List<User> getAll() {
-        Connection con = null;
-        PreparedStatement st = null;
-        ResultSet rs = null;
         List<User> userList = new ArrayList<>();
-
-        try {
-            con = pool.getConnection();
-            st = con.prepareStatement(sqlQueries.getString("GET_ALL_USERS"));
-            rs = st.executeQuery();
-
-            while (rs.next()) {
-                User a = new User();
-                a.setId(rs.getLong("id"));
-                a.setName(rs.getString("name"));
-                a.setPassword(rs.getString("pass"));
-                a.setTutor(rs.getBoolean("isTutor"));
-                userList.add(a);
+        try (
+                Connection con = pool.getConnection();
+                PreparedStatement st = con.prepareStatement(sqlQueries.getString("GET_ALL_USERS"));
+        ) {
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    String pass = rs.getString("pass");
+                    boolean tutor = rs.getBoolean("isTutor");
+                    User user = new User(name, pass, tutor);
+                    user.setId(rs.getLong("id"));
+                    userList.add(user);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null)
-                    rs.close();
-                if (st != null)
-                    st.close();
-                if (con != null)
-                    con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
         return userList;
     }
 
     @Override
     public void remove(Long id) {
-        Connection con = null;
-        PreparedStatement st = null;
-        try {
-            con = pool.getConnection();
-            st = con.prepareStatement(sqlQueries.getString("REMOVE_USER"));
+        try (
+                Connection con = pool.getConnection();
+                PreparedStatement st = con.prepareStatement(sqlQueries.getString("REMOVE_USER"));
+        ) {
             st.setLong(1, id);
             st.executeUpdate();
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (st != null)
-                    st.close();
-                if (con != null)
-                    con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
