577894745dab239d049a69f68f5581ae4b2db5c0
Guba Dmitriy <gudmian@gmail.com>
2018-03-22 14:28:16 +0300

    Make constants private

--- src/main/java/config/InitDB.java
+++ src/main/java/config/InitDB.java
@@ -13,6 +13,11 @@ import java.util.ResourceBundle;
 public class InitDB extends HttpServlet {
 
     private final static Logger log = LogManager.getLogger(InitDB.class);
+    private static final String SQL_QUERIES = "sql_queries";
+    private static final String H2_CREATE_USER = "h2.create.user";
+    private static final String H2_CREATE_TEST = "h2.create.test";
+    private static final String H2_CREATE_QUESTION = "h2.create.question";
+    private static final String H2_CREATE_ANSWER = "h2.create.answer";
 
     @Override
     public void init() throws ServletException {
@@ -22,11 +27,11 @@ public class InitDB extends HttpServlet {
         try {
             ConnectionPool pool = ConnectionPool.getInstance();
             con = pool.getConnection();
-            ResourceBundle res = ResourceBundle.getBundle("sql_queries");
-            String[] sqls = {res.getString("h2.create.user"),
-                    res.getString("h2.create.test"),
-                    res.getString("h2.create.question"),
-                    res.getString("h2.create.answer")};
+            ResourceBundle res = ResourceBundle.getBundle(SQL_QUERIES);
+            String[] sqls = {res.getString(H2_CREATE_USER),
+                    res.getString(H2_CREATE_TEST),
+                    res.getString(H2_CREATE_QUESTION),
+                    res.getString(H2_CREATE_ANSWER)};
             st = con.createStatement();
             for (String sql : sqls) {
                 st.executeUpdate(sql);
